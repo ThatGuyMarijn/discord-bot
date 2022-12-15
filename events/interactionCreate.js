66b7a802908ea7e162a93a79c8d66ff093a43ec8
ThatGuyMarijn <marijn.deijnen@outlook.com>
2022-12-15 06:58:35 +0100

    support for autocomplete

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,26 +3,41 @@ const { Events } = require("discord.js");
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
+        if (interaction.isChatInputCommand()) {
+            if (interaction.commandName === 'server') {
+                await interaction.deferReply({
+                    ephemeral: true
+                });
+            }
+    
+            const command = interaction.client.commands.get(interaction.commandName);
+    
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
+    
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(`Error executing ${interaction.commandName}`);
+                console.error(error);
+            }
+        } else if (interaction.isAutocomplete()) {
+            const command = interaction.client.commands.get(interaction.commandName);
 
-        if (interaction.commandName === 'server') {
-            await interaction.deferReply({
-                ephemeral: true
-            });
-        }
+            if (!command) {
+                if (!command) {
+                    console.error(`No command matching ${interaction.commandName} was found.`);
+                    return;
+                }
+            }
 
-        const command = interaction.client.commands.get(interaction.commandName);
-
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
-        }
-
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(`Error executing ${interaction.commandName}`);
-            console.error(error);
+            try {
+                await command.autocomplete(interaction);
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
 }
