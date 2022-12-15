a4bf331721335c3ec41490b6df9c9efbe090c5d1
ThatGuyMarijn <marijn.deijnen@outlook.com>
2022-12-15 06:46:38 +0100

    added choices

--- commands/amaru.js
+++ commands/amaru.js
@@ -6,8 +6,18 @@ module.exports = {
         .setDescription('Does Marijn love Amaru?')
         .addStringOption(option =>
             option.setName('input')
-                .setDescription('Is this a fact?')),
+                .setDescription('Is this a fact?')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Fact', value: 'true' },
+                    { name: 'Fake', value: 'false' }
+                )),
     async execute(interaction) {
-        await interaction.reply('Marijn loves Amaru!');
+        const input = interaction.options.getString('input');
+        if (input === 'true') {
+            await interaction.reply('Marijn loves Amaru!');
+        } else {
+            await interaction.reply('Marijn does not love Amaru! SIKE.');
+        }
     }
 };
